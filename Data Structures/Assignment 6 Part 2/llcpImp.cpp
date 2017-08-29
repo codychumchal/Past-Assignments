30ff4643f8be1e350e059ad5d3a8fc5796c7cd61
codychumchal <31426520+codychumchal@users.noreply.github.com>
2017-08-29 18:15:47 -0500

    Add files via upload

---
+++ Data Structures/Assignment 6 Part 2/llcpImp.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+#include <cstdlib>
+#include "llcpInt.h"
+using namespace std;
+
+int FindListLength(Node* headPtr)
+{
+   int length = 0;
+
+   while (headPtr != 0)
+   {
+      ++length;
+      headPtr = headPtr->link;
+   }
+
+   return length;
+}
+
+bool IsSortedUp(Node* headPtr)
+{
+   if (headPtr == 0 || headPtr->link == 0) // empty or 1-node
+      return true;
+   while (headPtr->link != 0) // not at last node
+   {
+      if (headPtr->link->data < headPtr->data)
+         return false;
+      headPtr = headPtr->link;
+   }
+   return true;
+}
+
+void InsertAsHead(Node*& headPtr, int value)
+{
+   Node *newNodePtr = new Node;
+   newNodePtr->data = value;
+   newNodePtr->link = headPtr;
+   headPtr = newNodePtr;
+}
+
+void InsertAsTail(Node*& headPtr, int value)
+{
+   Node *newNodePtr = new Node;
+   newNodePtr->data = value;
+   newNodePtr->link = 0;
+   if (headPtr == 0)
+      headPtr = newNodePtr;
+   else
+   {
+      Node *cursor = headPtr;
+
+      while (cursor->link != 0) // not at last node
+         cursor = cursor->link;
+      cursor->link = newNodePtr;
+   }
+}
+
+void InsertSortedUp(Node*& headPtr, int value)
+{
+   Node *precursor = 0,
+        *cursor = headPtr;
+
+   while (cursor != 0 && cursor->data < value)
+   {
+      precursor = cursor;
+      cursor = cursor->link;
+   }
+
+   Node *newNodePtr = new Node;
+   newNodePtr->data = value;
+   newNodePtr->link = cursor;
+   if (cursor == headPtr)
+      headPtr = newNodePtr;
+   else
+      precursor->link = newNodePtr;
+}
+
+bool DelFirstTargetNode(Node*& headPtr, int target)
+{
+   Node *precursor = 0,
+        *cursor = headPtr;
+
+   while (cursor != 0 && cursor->data != target)
+   {
+      precursor = cursor;
+      cursor = cursor->link;
+   }
+   if (cursor == 0)
+   {
+      cout << target << " not found." << endl;
+      return false;
+   }
+   if (cursor == headPtr) //OR precursor == 0
+      headPtr = headPtr->link;
+   else
+      precursor->link = cursor->link;
+   delete cursor;
+   return true;
+}
+
+bool DelNodeBefore1stMatch(Node*& headPtr, int target)
+{
+   if (headPtr == 0 || headPtr->link == 0 || headPtr->data == target) return false;
+   Node *cur = headPtr->link, *pre = headPtr, *prepre = 0;
+   while (cur != 0 && cur->data != target)
+   {
+      prepre = pre;
+      pre = cur;
+      cur = cur->link;
+   }
+   if (cur == 0) return false;
+   if (cur == headPtr->link)
+   {
+      headPtr = cur;
+      delete pre;
+   }
+   else
+   {
+      prepre->link = cur;
+      delete pre;
+   }
+   return true;
+}
+
+void ShowAll(ostream& outs, Node* headPtr)
+{
+   while (headPtr != 0)
+   {
+      outs << headPtr->data << "  ";
+      headPtr = headPtr->link;
+   }
+   outs << endl;
+}
+
+void FindMinMax(Node* headPtr, int& minValue, int& maxValue)
+{
+   if (headPtr == 0)
+   {
+      cerr << "FindMinMax() attempted on empty list" << endl;
+      cerr << "Minimum and maximum values not set" << endl;
+   }
+   else
+   {
+      minValue = maxValue = headPtr->data;
+      while (headPtr->link != 0)
+      {
+         headPtr = headPtr->link;
+         if (headPtr->data < minValue)
+            minValue = headPtr->data;
+         else if (headPtr->data > maxValue)
+            maxValue = headPtr->data;
+      }
+   }
+}
+
+double FindAverage(Node* headPtr)
+{
+   if (headPtr == 0)
+   {
+      cerr << "FindAverage() attempted on empty list" << endl;
+      cerr << "An arbitrary zero value is returned" << endl;
+      return 0.0;
+   }
+   else
+   {
+      int sum = 0,
+          count = 0;
+
+      while (headPtr != 0)
+      {
+         ++count;
+         sum += headPtr->data;
+         headPtr = headPtr->link;
+      }
+
+      return double(sum) / count;
+   }
+}
+
+void ListClear(Node*& headPtr, int noMsg)
+{
+   int count = 0;
+
+   Node *cursor = headPtr;
+   while (headPtr != 0)
+   {
+      headPtr = headPtr->link;
+      delete cursor;
+      cursor = headPtr;
+      ++count;
+   }
+   if (noMsg) return;
+   clog << "Dynamic memory for " << count << " nodes freed"
+        << endl;
+}
+
+
+// definition of SortedMergeRecur
+void SortedMergeRecur(Node*& xHead, Node*& yHead, Node*& zHead)
+{ 
+   //Base Cases
+   if(xHead == 0 && yHead == 0)
+      return;
+   if(xHead != 0 && yHead == 0)
+   {
+      zHead = xHead;
+      xHead = 0;
+      return;
+   }
+   if(xHead == 0 && yHead != 0)
+   {
+      zHead = yHead;
+      yHead = 0;
+      return;
+   }
+   else
+   {
+      if(yHead->data <= xHead->data)
+      {
+         zHead = yHead;
+         yHead = yHead->link;
+      }
+      else if(xHead->data <= yHead->data)
+      {
+         zHead = xHead;
+         xHead = xHead->link;
+      }
+      SortedMergeRecur(xHead, yHead, zHead->link);
+   }
+}
